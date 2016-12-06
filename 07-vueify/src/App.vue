3f20768f3e9bb000d158168c0b709f891edc3fe4
ginpei <ginpei@gmail.com>
2016-12-06 14:47:18 -0800

    vueify

---
+++ 07-vueify/src/App.vue
@@ -0,0 +1,42 @@
+<style>
+</style>
+
+<template>
+	<div class="container">
+		<h1>Vue TODO</h1>
+		<div>
+			<task-form :task="newTask"></task-form>
+		</div>
+		<hr />
+		<div class="taskListOperator">
+			<button v-on:click="delete_click" class="btn btn-default">Delete finished tasks</button>
+		</div>
+		<div class="list-group">
+			<task-item v-for="task in tasks" :task="task"></task-item>
+			<p v-show="tasks.length < 1" class="text-muted">No tasks. Yay!</p>
+		</div>
+	</div>
+</template>
+
+<script>
+const taskForm = require('./TaskForm.vue');
+const taskItem = require('./TaskItem.vue');
+const store = require('./store.js');
+
+module.exports = {
+	components: {
+		taskForm: taskForm,
+		taskItem: taskItem,
+	},
+	data: function() {
+		return store.state;
+	},
+	computed: {
+	},
+	methods: {
+		delete_click: function(event) {
+			this.tasks = this.tasks.filter(v=>!v.finished);
+		},
+	},
+};
+</script>
--- 07-vueify/src/main.js
+++ 07-vueify/src/main.js
@@ -1,66 +1,12 @@
-/* global window, Vue */
+const Vue = require('vue');
+const Vuex = require('vuex');
+Vue.use(Vuex);
 
-var store = new Vuex.Store({
-	state: {
-		newTask: { finished: false, name: '' },
-		tasks: [
-			{ finished: false, name: 'Buy milk 2L' },
-			{ finished: false, name: 'Call to Alice' },
-			{ finished: false, name: 'Return books' },
-		],
-	},
-	mutations: {
-		add: function(state) {
-			state.tasks.unshift(state.newTask);
-			state.newTask = { finished: false, name: '' };
-		},
-	},
-});
+const App = require('./App.vue');
 
-var taskForm = {
-	template: '#template-task-form',
-	props: [
-		'task',
-	],
-	methods: {
-		form_submit: function(event) {
-			if (!this.task.name) {
-				return;
-			}
-
-			store.commit('add');
-		},
-	},
-};
-
-var taskItem = {
-	template: '#template-task-item',
-	props: [
-		'task',
-	],
-	methods: {
-		edit_click: function(event) {
-			// update if not canceled
-			var newName = window.prompt('Task Name', this.task.name);
-			if (typeof newName === 'string') {
-				this.task.name = newName;
-			}
-		},
-	},
-};
-
-window.app = new Vue({
+new Vue({
 	el: '#app',
-	components: {
-		taskForm: taskForm,
-		taskItem: taskItem,
-	},
-	data: store.state,
-	computed: {
-	},
-	methods: {
-		delete_click: function(event) {
-			this.tasks = this.tasks.filter(v=>!v.finished);
-		},
+	render: function(createElement) {
+		return createElement(App);
 	},
 });
