c72c78005e7db8690b65d48ea856dc31b2ed87ab
ginpei <ginpei@gmail.com>
2016-12-11 00:41:45 -0800

    init

---
+++ 11-ajaxForm/src/App.vue
@@ -0,0 +1,18 @@
+<style>
+</style>
+
+<template>
+  <div class="container">
+    <h1>Ajax Form</h1>
+  </div>
+</template>
+
+<script>
+  var store = require('./store.js')
+
+  module.exports = {
+    data: function () {
+      return store.state
+    }
+  }
+</script>
