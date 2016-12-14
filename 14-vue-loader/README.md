7cd07a97a4e2b3538e76aa72bc57be77b8121593
ginpei <ginpei@gmail.com>
2016-12-13 20:11:36 -0800

    prepare

---
+++ 14-vue-loader/README.md
@@ -0,0 +1,3 @@
+# vue-loaderを通すとSASSやJade/Pugも使えるよ。（Vue.js始めるおれおれアドベントカレンダー2016 – 14日目）
+
+http://ginpen.com/2016/12/14/vue-loader/
---
+++ 14-vue-loader/src/store.js
@@ -0,0 +1,18 @@
+const Vuex = require('vuex')
+
+module.exports = new Vuex.Store({
+  state: {
+    newTask: { finished: false, name: '' },
+    tasks: [
+			{ finished: false, name: 'Buy milk 2L' },
+			{ finished: false, name: 'Call to Alice' },
+			{ finished: false, name: 'Return books' }
+    ]
+  },
+  mutations: {
+    add: function (state) {
+      state.tasks.unshift(state.newTask)
+      state.newTask = { finished: false, name: '' }
+    }
+  }
+})
